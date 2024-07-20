a9ac12e0e8467a5698980e45f6ed180c1153f776
Rob Speed <speed.rob@gmail.com>
2024-07-20 13:14:13 -0400

    Simplifies the settings module and enables code completions.

--- reservation_clock/settings.py
+++ reservation_clock/settings.py
@@ -1,15 +1,27 @@
-from copy import copy
+from copy import deepcopy
 from io import open as open_file
 from json import load as read_json, dump as write_json
-
-from typing import Any, TypedDict
 
 
 __all__ = 'Settings', 'settings'
 
 
 
-class SettingsFileSchema(TypedDict):
+class Settings:
+	"""Settings for the Reservation Clock app."""
+
+	"""Path to the file that stores settings."""
+	FILE_PATH: str = 'settings.json'
+
+	DEFAULTS: dict = {
+		'wlans': [],
+		'google_api_credentials': {},
+		'google_api_scopes': [],
+		'google_calendar_id': '',
+		'display_gpio_pins': [],
+		'display_pixel_count': 5 * 7
+	}
+
 	"""SSIDs and passwords of wireless networks to attempt to access."""
 	wlans: list[list[str, str]]
 
@@ -29,70 +41,35 @@ class SettingsFileSchema(TypedDict):
 	display_pixel_count: int
 
 
-
-class Settings:
-	"""Settings for the Reservation Clock app."""
-
-	"""Path to the file that stores settings."""
-	FILE_PATH: str = 'settings.json'
-
-	DEFAULTS: SettingsFileSchema = {
-		'wlans': [],
-		'google_api_credentials': {},
-		'google_api_scopes': [],
-		'google_calendar_id': '',
-		'display_gpio_pins': [],
-		'display_pixel_count': 5 * 7
-	}
-
-	KEYS: tuple = tuple(DEFAULTS.keys())
-
-	_settings: SettingsFileSchema
-
-
 	def __init__ (self) -> None:
 		"""Read the settings from storage."""
 
-		self._settings = self.DEFAULTS
-		self._dirty = False
+		saved_settings: dict = self._saved_settings()
 
-		self.load()
+		for key, default_value in self.DEFAULTS.items():
+			# Set local attributes to a saved value or a default
+			if hasattr(saved_settings, key):
+				setattr(self, key, saved_settings[key])
+			else:
+				setattr(self, key, deepcopy(default_value))
 
 
-	def load (self):
+	def _saved_settings (self) -> dict:
+		"""Loads the settings from storage."""
 		try:
 			with open_file(self.FILE_PATH, 'r') as f:
-				self._settings = read_json(f)
+				return read_json(f)
 
 		except (OSError, ValueError):
 			# Stick with the defaults
-			pass
+			return {}
 
 
 	def flush (self) -> None:
 		"""Write the settings to storage."""
 
 		with open_file(self.FILE_PATH, 'w') as f:
-			write_json(self._settings, f)
-
-
-	def __getattr__ (self, key: str) -> Any:
-		try:
-			return self._settings[key]  # type: ignore
-		except KeyError:
-			pass
-
-		try:
-			return copy(self.DEFAULTS[key])  # type: ignore
-		except KeyError:
-			raise AttributeError(key)
-
-
-	def __setattr__ (self, key: str, value: Any) -> None:
-		if key not in self.KEYS:
-			return super().__setattr__(key, value)
-
-		self._settings[key] = value  # type: ignore
+			write_json(f, {key: getattr(self, key) for key in self.DEFAULTS.keys()})
 
 
 
